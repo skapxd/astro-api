04db29bac84718cf7084c5bfddb9ca5619184747
Manuel <hbiaser132@gmail.com>
2024-09-07 11:29:37 -0500

    test

--- src/pages/api/hello-world.ts
+++ src/pages/api/hello-world.ts
@@ -2,6 +2,8 @@ import type { EndpointHandler } from "astro";
 export const prerender = false;
 
 // Outputs: /builtwith.json
-export async function GET({ params, request }) {
+export async function GET(props: any) {
+  console.log({ props });
+
   return new Response("Hello, world!");
 }
